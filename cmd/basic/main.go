73b78d05616e5b62046369885f30d10f673aa3ff
Anh Le <letuananh.ttmmt.ctu@gmail.com>
2020-11-09 11:48:21 +0700

    Simple pdf file

--- cmd/basic/main.go
+++ cmd/basic/main.go
@@ -1,8 +1,23 @@
 // Link: https://courses.calhoun.io/lessons/les_goph_127
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+var (
+	output = "output/p1.pdf"
+)
 
 func main() {
-	fmt.Println("Hello Basic")
+	pdf := gofpdf.New("P", "mm", "A4", "Arial")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 18)
+	pdf.Cell(40, 10, "Hello World")
+
+	if err := pdf.OutputFileAndClose(output); err != nil {
+		log.Fatalf("something went wrong: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/captain-corgi/building-pdfs
 
 go 1.15
 
-require github.com/captain-corgi/go-mod-example v1.0.0
+require (
+	github.com/captain-corgi/go-mod-example v1.0.0
+	github.com/jung-kurt/gofpdf v1.16.2
+)
