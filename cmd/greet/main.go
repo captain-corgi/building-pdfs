f213af24a210a10a0c01222bde7e732d939a08b7
Anh Le <letuananh.ttmmt.ctu@gmail.com>
2020-11-09 11:04:03 +0700

    Init go mod

---
+++ cmd/greet/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/captain-corgi/go-mod-example/greet"
+
+func main() {
+	greet.HelloHuman("Anh Le")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/captain-corgi/building-pdfs
+
+go 1.15
+
+require github.com/captain-corgi/go-mod-example v1.0.0
